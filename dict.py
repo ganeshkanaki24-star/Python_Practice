e72a49452de232f26fc45c930cc11e4fa87442a7
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-02 11:00:24 +0530

    new experiment

--- dict.py
+++ dict.py
@@ -14,3 +14,21 @@ copied_dict['name'] = 'jane'
 print("Original:", Original_dict)
 print("Copied:", copied_dict)
 
+
+# marge the dictionery
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+dict1.update(dict2)
+print(dict1)
+
+# squere the values in dictionery
+square_dict ={
+    'a':1,
+    'b':2,
+    'c':3
+}
+print({k:v**2 for k,v in square_dict.items()})
+
+
+dict_comp ={k:k for k in range(0,10) if k<=4}
+print(dict_comp)
