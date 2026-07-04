0b7efa196796cb0f6e6135dc4f00eb8246c3d5ba
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-04 09:41:37 +0530

    new generator experiment

--- generator.py
+++ generator.py
@@ -7,3 +7,16 @@ gen = squares(5)
 print(next(gen))
 print(next(gen))
 print(next(gen))
+
+# factorial using generator
+def factorial_generator(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+        yield result
+
+fact_gen = factorial_generator(5)
+for value in fact_gen:
+    print(value)
+
+
