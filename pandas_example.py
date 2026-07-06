ec54c4f17fccc8e819c96e2fa55f2dec373d0054
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-06 12:08:57 +0530

    pandas examples

--- pandas_example.py
+++ pandas_example.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+'''
 data = {
     "dept": ["HR", "HR", "IT","IT"],
     "salary" : [10000, 15000, 20000, 25000]
@@ -7,3 +7,12 @@ data = {
 df = pd.DataFrame(data)
 result = df.groupby("dept")["salary"].mean()
 print(result)
+'''
+# find greter than 70 marks using pandas and lambda
+student = {
+    "name": ['Ganesh','Digvijay','Ruturaj','Patil','vijay'],
+    "marks": [77, 60, 80, 55, 90]
+}
+df = pd.DataFrame(student)
+show = df[df['marks'].apply(lambda x : x < 70)]
+print(show)
