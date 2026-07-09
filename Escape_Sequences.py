5e5da2b3339c80cb061422e156b89e9092a487c4
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-09 23:35:38 +0530

    new experiment

--- Escape_Sequences.py
+++ Escape_Sequences.py
@@ -2,3 +2,5 @@ print("Your a Good Boy \n if your Girl Your Good Girl")
 # Comments 
 # Single line comment
 print("The comments is only for remember, you that code it cannot show in output")
+
+print("I am Ganesh And i am \"Greate Student\" \n and i am good boy too")
