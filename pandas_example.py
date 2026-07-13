8d3b1fd6ba2ff1527afcf615b388f59ba794d561
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-13 13:31:33 +0530

    New Practice

--- pandas_example.py
+++ pandas_example.py
@@ -1,5 +1,6 @@
-import pandas as pd
 '''
+import pandas as pd
+
 data = {
     "dept": ["HR", "HR", "IT","IT"],
     "salary" : [10000, 15000, 20000, 25000]
@@ -7,6 +8,7 @@ data = {
 df = pd.DataFrame(data)
 result = df.groupby("dept")["salary"].mean()
 print(result)
+'''
 '''
 # find greter than 70 marks using pandas and lambda
 student = {
@@ -16,3 +18,24 @@ student = {
 df = pd.DataFrame(student)
 show = df[df['marks'].apply(lambda x : x < 70)]
 print(show)
+'''
+
+import csv
+data = [
+    ['name', 'age', 'city'],
+    ['Alice', 30, 'New York'],
+    ['Bob', 25, 'Los Angeles'],
+    ['Charlie', 35, 'Chicago']
+]
+
+# Create the file
+with open('data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+
+print("CSV file created successfully.")
+
+with open('data.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
