66de4867f32d625184736dd2a8ba86f17b1d82f6
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-18 22:54:07 +0530

    variable

--- variables.py
+++ variables.py
@@ -1,3 +1,4 @@
+'''
 a = complex(7, 2)
 b = True
 c = "Ganesh"
@@ -10,3 +11,22 @@ print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+
+#output
+# (7+2j)
+# True
+# (16+2j)
+# <class 'complex'>
+# <class 'bool'>
+# <class 'str'>
+# <class 'NoneType'>
+'''
+#tuple, list, dictionary
+list = [45, 8.5, [-4, 5], ["apple", "banana"]]
+print(list)
+
+tuple = (("Parrot", "Sparrow"), ("Lion", "Tiger"))
+print(tuple)
+
+dict = {"name":"Ganesh", "age":22, "canvote":True}
+print(dict)
