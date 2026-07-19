5d3c444c3c3c47f0696f9e2e6536428cd37c5f85
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-19 21:34:04 +0530

    Day 1

---
+++ Day 1/q2_personal_info.py
@@ -0,0 +1,15 @@
+# Question 2 - Personal Information
+
+# Take input from the user:
+
+# Name
+# Age
+# City
+
+Name = input("Enter Name: ")
+Age = int(input("Enter your Age: "))
+City = input("Enter your City: ")
+
+print(Name)
+print(Age)
+print(City)
