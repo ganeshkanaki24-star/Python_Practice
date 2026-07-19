bc8e2e3882ccad2b886ac3eb977186caab17bcd6
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-19 21:45:36 +0530

    Day 1

---
+++ Day 1/q4_calculator.py
@@ -0,0 +1,17 @@
+# Question 4 - Simple Calculator
+
+# Take two numbers.
+
+# Print:
+
+# Addition
+# Subtraction
+# Multiplication
+# Division
+
+a = int(input("Enter 1st Number: "))
+b = int(input("Enter 2nd Number: "))
+print("Addition: ",a + b)
+print("Subtraction: ",a - b)
+print("Multiplication: ",a * b)
+print("Division: ",a/b)
