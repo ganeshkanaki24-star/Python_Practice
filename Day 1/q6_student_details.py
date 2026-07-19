32d201fd2ad87abb9561ce515855950589dcd0ad
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-19 22:43:31 +0530

    Day 1

---
+++ Day 1/q6_student_details.py
@@ -0,0 +1,16 @@
+# Question 6 - Student Details
+
+# Input
+
+# Name
+# Roll Number
+# College
+# Course
+name = input("Enter Name: ")
+rollno = int(input("Enter Roll No.: "))
+college = input("Enter College Name: ")
+course = input("Enter Course: ")
+print(name)
+print(rollno)
+print(college)
+print(course)
