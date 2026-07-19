44bebe2287323aa6d2aa42057a95b30154afaac3
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-19 23:05:21 +0530

    Day 1

---
+++ Day 1/q7_rectangle.py
@@ -0,0 +1,17 @@
+# Question 7 - Area of Rectangle
+
+# Take
+
+# Length
+# Breadth
+
+# Print
+
+# Area = ...
+# Perimeter = ...
+Length = float(input("Enter Length: "))
+Breadth = float(input("Enter Breadth: "))
+Area = Length * Breadth
+Perimeter = 2 * (Length + Breadth)
+print(Area)
+print(Perimeter)
