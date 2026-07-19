3f5629c31b4e56c88d74c1f599977642af326872
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-19 21:58:58 +0530

    Day 1

---
+++ Day 1/q5_swap_numbers.py
@@ -0,0 +1,23 @@
+# Question 5 - Swap Two Numbers
+
+# Input:
+
+# 10
+# 20
+
+# Output
+
+# Before Swapping
+# A = 10
+# B = 20
+
+# After Swapping
+# A = 20
+# B = 10
+a = 10
+b = 20
+c = a 
+a = b
+b = c
+print(a)
+print(b)
