fe358d63aba072af2cb1246f19d7ba47fd98740f
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-21 22:59:05 +0530

    Day 1

---
+++ Day 1/q10_percentage.py
@@ -0,0 +1,25 @@
+# Question 10 - Marks Percentage
+
+# Input marks of
+
+# English
+# Maths
+# Science
+# Computer
+# Hindi
+
+# Find
+
+# Total
+# Percentage
+
+English = 77
+Maths = 87
+Science = 65
+Computer = 92
+Hindi = 95
+
+Total = English + Maths + Science + Computer + Hindi
+Percentage = (Total/500) * 100
+print(Total)
+print(Percentage)
