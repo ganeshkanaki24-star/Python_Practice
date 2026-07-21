755b204a73da2e5ffde1053c7c1e47ecf46ebc6f
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-07-21 22:50:58 +0530

    Day 1

---
+++ Day 1/q9_temperature.py
@@ -0,0 +1,13 @@
+# Question 9 - Temperature Converter
+
+# Take Celsius.
+
+# Convert into Fahrenheit.
+
+# Formula
+
+# F = (C × 9/5) + 32
+
+C = int(input("Enter the Celsius: "))
+F = (C * 9/5) + 32
+print(F)
