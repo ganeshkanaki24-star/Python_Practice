976b1258d83597a21c6194495d5030eb7729f3fe
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-08-23 10:59:17 +0530

    Experiment of calculater

---
+++ calculater.py
@@ -0,0 +1,9 @@
+# Creating the calculator 
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition is: ", a + b)
+print("Subtraction is: ", a - b)
+print("Multiplication is: ",a*b)
+print("Dividation is: ", a/b)
+print("Modulation is: ", a%b)
