f07df5244dea484c94e4c201168b110b6192eec9
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-08-27 09:53:26 +0530

    Type casting

---
+++ Day 3/scenario_02_shopping_bill.py
@@ -0,0 +1,11 @@
+# Scenario 2 — Shopping Bill 🛒
+
+# A shopkeeper enters the product price and quantity using input().
+
+price_value = input()
+quantity_value = input()
+
+price=int(price_value)
+quantity=int(quantity_value)
+total = price * quantity
+print(total)
