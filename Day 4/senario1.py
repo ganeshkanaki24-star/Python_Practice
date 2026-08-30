66221f22c3931d969b750d31ec18279402d6841d
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-08-30 22:08:12 +0530

    user input

---
+++ Day 4/senario1.py
@@ -0,0 +1,18 @@
+# 1. Student Details
+
+# Take input for:
+
+# Student name
+# Age
+# College name
+# Course
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+clg_name = input("Enter your college name: ")
+course = input("Enter your course: ")
+
+print(name)
+print(age)
+print(clg_name)
+print(course)
