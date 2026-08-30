438131551a00301a36a4bd690d24476992cb9798
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-08-30 22:14:55 +0530

    user input

---
+++ Day 4/senario2.py
@@ -0,0 +1,17 @@
+# 2. Simple Calculator
+
+# Take two numbers from the user and display:
+
+# Addition
+# Subtraction
+# Multiplication
+# Division
+
+a = int(input("Enter a number 1: "))
+b = int(input("Enter a number 2: "))
+
+print("Addition: ", a + b)
+print("Substraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a/b)
+
