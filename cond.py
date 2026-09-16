740ec5eea3873eb48a5d6d64e2dd4f4ed5441c9b
ganeshkanaki24-star <ganeshkanaki24@gmail.com>
2026-09-16 23:55:06 +0530

    Condition

--- cond.py
+++ cond.py
@@ -1,3 +1,6 @@
-age = 19
-if age > 18:
-    print("Your adult")
+age = int(input("Enter your Age: "))
+# if age > 18:
+#     print("Your adult")
+
+status = "Adult" if age >= 18 else "Miner"
+print(status)
